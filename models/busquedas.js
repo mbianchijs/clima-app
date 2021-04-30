6cf76f8f603104c98247af8e525afeff99e2505f
Jose Martinez <mbianchijs@gmail.com>
2021-04-30 00:16:58 -0400

    Getter urlParams y consulta axios

--- models/busquedas.js
+++ models/busquedas.js
@@ -6,15 +6,34 @@ class Busquedas {
 
     }
 
+    get urlParams() {
+
+        return {
+            'access_token': 'pk.eyJ1IjoibWJpYW5jaGlqcyIsImEiOiJja28zbzlodHQwYnk4Mm9vbnN2cW1ocTd0In0.hZYOBgCbi5buoXTO06gOrQ',
+            'limit': 7,
+            'language': 'es'
+        }
+
+    }
+
     async ciudad( lugar = '' ) {
 
         try {
             
-            const res = await axios.get('');
+            const instancia = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ lugar }.json`,
+                params: this.urlParams
+
+            })
+
+            const res = await instancia.get();
+
+            console.log(res.data)
 
             return [];
 
         } catch (error) {
+
             return [];
         }
 
