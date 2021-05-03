32dceb4b483062391ca41b9bbe91850508daec40
Jose Martinez <mbianchijs@gmail.com>
2021-05-02 23:43:34 -0400

    Comentario clase Busquedas

--- models/busquedas.js
+++ models/busquedas.js
@@ -12,16 +12,18 @@ class Busquedas {
 
     }
     
+    // Parámetros para consulta Mapbox
     get urlParamsMapbox() {
-
+        
         return {
             'access_token': process.env.MAPBOX_KEY,
             'limit': 7,
             'language': 'es'
         }
-
+        
     }
 
+    // Parámetros para consulta Open Weather
     get urlParamsOW() {
 
         return {
@@ -32,48 +34,47 @@ class Busquedas {
 
     }
 
+    // Capitalizado del lugar/ciudad para mostrar en el historial
     get capitalizado() {
 
         return this._historial.map( registro => {
             let palabras = registro.lugar.split(" ");
             palabras = palabras.map(palabra => palabra[0].toUpperCase() + palabra.substring(1));
-            return (
-                {
-                    lugar: palabras.join(' '),
-                    ultimaConsulta: registro.ultimaConsulta
-
-                }
-            );
-        } )
-
+            return ({
+                lugar: palabras.join(' '),
+                ultimaConsulta: registro.ultimaConsulta
+            });
+        })
     }
 
+    // Consulta el lugar/ciudad al API Mapbox y devuelve las coincidencias
     async ciudad( lugar = '' ) {
-
+        
         try {
             
             const instancia = axios.create({
                 baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ lugar }.json`,
                 params: this.urlParamsMapbox
-
+                
             })
-
+            
             const res = await instancia.get();
-
+            
             return res.data.features.map( lugar => ({
                 id: lugar.id,
                 nombre: lugar.place_name,
                 longitud: lugar.center[0],
                 latitud: lugar.center[1]
             }))
-
+            
         } catch (error) {
-
+            
             return [];
         }
-
+        
     }
-
+    
+    // Consulta factores climáticos al API Open Weather segun latitud y longitud
     async climaCiudadLugar( lat, lon ) {
 
         try {
@@ -102,20 +103,19 @@ class Busquedas {
 
     }
 
+    // Anexa última coincidencia en memoria y guarda todo en DB
     guardarHistorial( lugar = '' ) {
 
         const obj = {
-            
             lugar: lugar.toLowerCase(),
             ultimaConsulta: new Date()
-
         }
 
         this._historial.unshift(obj);
-
         insertHistorialDB(this._historial);
     }
 
+    // Carga en memoria el historial guardado en la DB
     leerHistorial() {
 
         const data = leerHistorialDB();
